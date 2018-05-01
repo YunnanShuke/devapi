03e916c03e57ebe8c174775a7fe89eef4da745b4
sixosio <sixos@sixos.io>
2018-05-01 15:55:39 +0800

    add get banner url

--- app/Http/Controllers/GetBannerController.php
+++ app/Http/Controllers/GetBannerController.php
@@ -3,9 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class GetBannerController extends Controller
 {
     //
+    public function getCostom(){
+        $dbBanner = DB::table('banner_url') -> get();
+        //dd($dbBanner);
+        foreach ($dbBanner as $banner){
+            $getBanner['url'] = $banner -> url;
+            $getBanner['pos'] = $banner -> pos;
+            $getBanner['remark'] = $banner -> remark;
+        }
+        return $getBanner;
+    }
 
 }
