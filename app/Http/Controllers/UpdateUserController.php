a6fe745b960f027654713e9dc8ab19a1d7faa2da
sixosio <sixos@sixos.io>
2018-05-10 21:53:29 +0800

    add update users

---
+++ app/Http/Controllers/UpdateUserController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UpdateUserController extends Controller
+{
+    //主方法
+    public function index(){
+
+    }
+
+    //次方法
+    public function test(){
+
+    }
+}
